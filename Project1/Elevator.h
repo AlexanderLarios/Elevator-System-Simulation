1189ad759c89cbc576ccc1236c7469fec4afc562
Alandric <acjpq4@mail.umkc.edu>
2017-06-30 13:40:30 -0500

    Update Elevator.h

--- Project1/Elevator.h
+++ Project1/Elevator.h
@@ -1,5 +1,41 @@
 //Elevator.h
-//Elevator class header.
+
+#include Simulation.h
+#include User Interface.h
 
 #pragma once
+
+//Elevator class header.
+class Elevator {
+public:
+Elevator() {}
+
+Elevator(int floors):
+floorCalled(), direction(1)
+currentFloor(0),
+defaultFloor(floors), 
+ 
+//This moves the elevator  in the dsired direction when the user pressed the button for the desired floor
+void gotoFloor(bool show_all) {
+if(buttonCalls.empty()) {
+return;
+}  
+
+
+  if (show) {
+   cout << "The elevator has made its PICKUP" << endl;
+  }
+}
+ int getdefaultFloor() {
+  return defaultFloor;
+}
+ 
+ private: 
+ int currentFloor;
+ bool direction;
+ int levels;
+ int defaultFloor1;
+ int defaultFloor2;
+ int defaultFloor3;
+ 
 //Add definitions here
