c2b58451fd72046db720b919c66cf91fc12f7da4
Alandric <acjpq4@mail.umkc.edu>
2017-07-05 15:22:34 -0500

    Create User.h

---
+++ Project1/User.h
@@ -0,0 +1,14 @@
+#include "elevator.h"
+
+struct User{
+public:
+User(){};
+User(bool dropoff, bool direction, int defaultFloor, int currentFloor);
+int levels;
+int currentFloor;
+int direction;
+int defaultFloor;
+
+};
+
+
