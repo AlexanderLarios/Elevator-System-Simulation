96a3a158530ea385deed18491668c3addd479a53
Alandric <acjpq4@mail.umkc.edu>
2017-07-05 15:11:27 -0500

    Create Simulator.h

---
+++ Project1/Simulator.h
@@ -0,0 +1,21 @@
+#include "Elevator.h"
+#include <iostream>
+
+using namespace std;
+
+class Simulation{
+
+public:
+
+Simulator (int levels, int choice)
+Elevator (floors, arrival);
+
+void run_simulation();
+void moveElevator();
+
+private:
+void Simulation:: run_simulation(){
+}
+void Simulation::moveElevator(){
+}
+
