754dcc67c31d79e5fb15b96e1e2c82c6081ccf25
Michael Essmyer <mde9rb@mail.umkc.edu>
2017-07-06 18:29:41 -0500

    Update Simulation.cpp

--- Project1/Simulation.cpp
+++ Project1/Simulation.cpp
@@ -2,7 +2,7 @@
 // CS303 Project 1
 
 #include "Simulator.h"
-
+#include "Random"
 
  void Simulation :: Simulation (int elevatorFloor_, int MaxTurns_, double Frequency_)
  {  
@@ -10,3 +10,20 @@
     MaxTurns = MaxTurns_;
     Frequency = Frequency_;
  }
+
+void Simulation :: Simulate ()
+{  int turns = 0;
+   while(turns < MaxTurns)
+   {  //random number to generate a new user
+      New User();
+     
+     //random numbers to generate starting and ending floors
+      int start;
+      int end;
+      
+    
+     
+      Elevator.process(); 
+       turns++;
+   }
+}
