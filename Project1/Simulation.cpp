9e97f4aa9f624f19a698ea2180be73e42a7825ee
Michael Essmyer <mde9rb@mail.umkc.edu>
2017-07-08 17:20:09 -0500

    Update Simulation.cpp

--- Project1/Simulation.cpp
+++ Project1/Simulation.cpp
@@ -10,7 +10,32 @@
     MaxTurns = MaxTurns_;
     Frequency = Frequency_;
  }
-
+void Simulation :: Simulate ()
+{  int turns = 0;
+   while(turns < MaxTurns)
+   {  //Generate a new user
+      User theUser = new User;
+    
+     //random numbers to generate starting and ending floors
+      int start = random();
+      int end = random();
+    
+    //determine direction from rand nums
+    int direction_;
+     if(end-start > 0) direction_ = 1;
+     else direction_ = -1;
+    
+    //assign random numbers to the user
+      theUser(direction_, start, end);
+    
+    //send the user info to the elevator to be processed
+      Elevator.process(theUser); 
+    
+    //Increment the number of turns
+      turns++;
+   }
+}
+/*
 void Simulation :: Simulate ()
 {  int turns = 0;
    while(turns < MaxTurns)
@@ -27,3 +52,4 @@ void Simulation :: Simulate ()
        turns++;
    }
 }
+*/
