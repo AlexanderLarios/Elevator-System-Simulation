1adb8bb29d67c435e1b500bee7ac359f9c0a2b22
Michael Essmyer <mde9rb@mail.umkc.edu>
2017-07-08 19:46:19 -0500

    Update Simulation.cpp

--- Project1/Simulation.cpp
+++ Project1/Simulation.cpp
@@ -10,29 +10,31 @@
     MaxTurns = MaxTurns_;
     Frequency = Frequency_;
  }
-void Simulation :: Simulate ()
-{  int turns = 0;
-   while(turns < MaxTurns)
-   {  //Generate a new user
+bool Simulation :: genuser ()
+{    //Generate a new user
+   if(rand() % (1 - 0 + 1)) == 1)
+   {
       User theUser = new User;
     
      //random numbers to generate starting and ending floors
-      int start = random();
-      int end = random();
+      int start = int(std::random()%nfloor_num);
+      int end = int(std::random()%nfloor_num);
     
-    //determine direction from rand nums
-    int direction_;
-     if(end-start > 0) direction_ = 1;
-     else direction_ = -1;
-    
-    //assign random numbers to the user
-      theUser(direction_, start, end);
+    //pushback user contructor into the list container
+   }
+}
+void Simulation :: Simulate ()
+{  int turns = 0;
+   while(turns < MaxTurns)
+   { if(genuser()){
     
     //send the user info to the elevator to be processed
-      Elevator.process(theUser); 
+      Elevator.process(); 
     
     //Increment the number of turns
-      turns++;
+      turns++;}
+    else
+     turns++;
    }
 }
 /*
