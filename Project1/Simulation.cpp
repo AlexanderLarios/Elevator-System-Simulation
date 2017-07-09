a1ece677d55264d2f33dc0276a07e84f78f919e9
Michael Essmyer <mde9rb@mail.umkc.edu>
2017-07-08 19:59:36 -0500

    Update Simulation.cpp

--- Project1/Simulation.cpp
+++ Project1/Simulation.cpp
@@ -12,11 +12,11 @@
  }
 bool Simulation :: genuser ()
 {    //Generate a new user
-   if(rand() % (1 - 0 + 1)) == 1)
+   if(rand() % (2)) == 0)
    {
      //random numbers to generate starting and ending floors
-      int start = int(std::random()%nfloor_num);
-      int end = int(std::random()%nfloor_num);
+      int start = int(std::random()%floors);
+      int end = int(std::random()%floors);
     
     //pushback user contructor into the list container
     Passengers.push_back(User(start, end));
@@ -36,29 +36,4 @@ void Simulation :: Simulate ()
      turns++;
    }
 }
-/*
-void Simulation :: Simulate ()
-{  int turns = 0;
-   while(turns < MaxTurns)
-   {  //Generate a new user
-      User theUser = new User;
-    
-     //random numbers to generate starting and ending floors
-      int start = random();
-      int end = random();
-    
-    //determine direction from rand nums
-    int direction_;
-     if(end-start > 0) direction_ = 1;
-     else direction_ = -1;
-    
-    //assign random numbers to the user
-      theUser(direction_, start, end);
-    
-    //send the user info to the elevator to be processed
-      Elevator.process(theUser); 
-    
-    //Increment the number of turns
-      turns++;
-   }
-}
+
