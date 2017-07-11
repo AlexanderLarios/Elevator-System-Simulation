ff3184eb2cd3f4795b86ae1d0633c5d5be15738d
Alexander Larios <alarios91@gmail.com>
2017-07-11 18:35:20 -0500

    updated user class
    add member functions.

--- Project1/User.cpp
+++ Project1/User.cpp
@@ -1,13 +1,30 @@
 #include "User.h"
 
-User :: User(int defaultFloor, int finalFloor, int direction_){
+User::User(int defaultFloor, int finalFloor, int direction_){
     startFloor = defaultFloor;
     endFloor = finalFloor;
     pickedUp = false;
     direction = direction_;
     }
 
-void User:: setWT(int wt){
+
+void User::setWT(int wt){
     waitingTime = wt;
+    }
+
+
+int User::getWT(){
+    return waitingTime;
 }
 
+int User::getStartFloor(){
+    return startFloor;
+}
+
+int User::getEndFloor(){
+    return endFloor;
+}
+
+int User::getDirection(){
+    return direction;
+}
--- Project1/User.h
+++ Project1/User.h
@@ -1,18 +1,21 @@
 #pragma once
 class User{
-public:
-    
-    User();
-    User(int defaultFloor, int finalFloor, int direction_);
-    bool pickedUp;
-    void setWT(int wt);
-    
 private:
     int waitingTime;
-    int defaultFloor;
-    int finalFloor;
     int direction;
     int startFloor;
     int endFloor;
+    bool pickedUp;
+    
+public:
+    
+    User(int defaultFloor, int finalFloor, int direction_);
+    void setWT(int wt);
+    int getWT();
+    int getStartFloor();
+    int getEndFloor();
+    int getDirection();
+    
+
 };
 
