0756330e36bacf2cddb93600c5c9ab1143ab5957
Michael Essmyer <mde9rb@mail.umkc.edu>
2017-07-10 19:28:19 -0500

    Update User.h

--- Project1/User.h
+++ Project1/User.h
@@ -1,3 +1,4 @@
+//Similar to Passenger.h for Airline simulation
 #include "elevator.h"
 #include "Random.h"
 struct User {
@@ -8,7 +9,7 @@ public:
       int TurnsSinceSpawn
       int defaultFloor; //should be type random instead of int
       int finalFloor;//should be type random instead of int
-      Random my_random; //making those random should allow us to get rid of this line...
+      Random my_random; //making those^^^ random should allow us to get rid of this line...
   
       int direction;
       bool pickedUp;
