ac10b55d18e93b2a19b555ef1d1a64a43126a4c1
Alexander Larios <alarios91@gmail.com>
2017-07-12 00:53:28 -0500

    Random edit

--- Project1/Random.h
+++ Project1/Random.h
@@ -1,11 +1,11 @@
 #include <random>
 // semi random int between 1and 100
-int Random() {
+int randomInt() {
     int randoPoint = rand() % 100 + 1;
     return randoPoint;
 }
 // semi random number in range bet
-int Random(int floors) {
+int randomFloor(int floors) {
     int randoPoint = rand() % floors + 1;
 
   
