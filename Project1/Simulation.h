b5b1f32cbf957e6bbedefca2c596f3cd788d5821
Alexander Larios <alarios91@gmail.com>
2017-07-12 00:50:08 -0500

    Simulation.h

--- Project1/Simulation.h
+++ Project1/Simulation.h
@@ -3,18 +3,18 @@
 #pragma once
 class Simulation {
 private:
+	int MaxTurn;
     int clock;
-    int arrival_time;
-    int waiting_time; //max_processing_time in simulator
-    int my_random;
+	int waitTime;
+	int randomInt;
     double Frequency;
     std::list<User> simPassengers;
    
 public:
     Simulation(int elevatorFloor_, int MaxTurns_, double Frequency_);
     int elevatorFloor;
-    int MaxTurn;
-    int waitTime;
+    
+  
     int get_arrival_time (){ return arrival_time;}
     int get_waiting_time (){ return waiting_time;}
     void Simulate();
